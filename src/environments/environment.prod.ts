03a093ede63c55a967060ef5698ce56618e92653
cavidann <cavi98@icloud.com>
2019-02-26 19:30:41 +0400

    ended

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    spaceId: 'cro6zeemhouj',
+    token: '6c1e82ba9764ee0c80986cae797a9d71c47d13bdb86cbbddc58d1488bafa2adb'
+  }
 };
